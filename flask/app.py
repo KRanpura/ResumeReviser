633c9b0aec5d91ec6ddf5d68af170a3e054bfbc8
Khushi <ranpurakhushi@gmail.com>
2023-11-11 05:12:41 -0500

    pdf upload works

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, redirect, request, session, url_for
+from flask import Flask, render_template, redirect, request, session, url_for, send_file
+import os
 from init_db import get_db
+from flask import send_from_directory
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'pdf'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("mainpage.html")
 
 @app.route("/login")
 def login():
@@ -53,3 +63,33 @@ def signup():
         return redirect(url_for("mainpage"))
 
     return render_template("signup.html")
+
+@app.route("/mainpage")
+def mainpage():
+    return render_template("mainpage.html")
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'pdf_file' not in request.files:
+        return render_template('mainpage.html', error='No file part')
+
+    file = request.files['pdf_file']
+
+    if file.filename == '':
+        return render_template('mainpage.html', error='No selected file')
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        return render_template('mainpage.html', pdf_path=filepath, filename = filename)
+
+    return render_template('mainpage.html', error='Invalid file type')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
