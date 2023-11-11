ca7907eef51938e05597306c9a0e7885548313ee
Khushi <ranpurakhushi@gmail.com>
2023-11-11 03:21:19 -0500

    fixed navigation errors

--- flask/app.py
+++ flask/app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect(url_for('login'))
+    return render_template("index.html")
 
 @app.route("/login")
 def login():
     return render_template ("login.html" )
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 # @app.route("/hello/<name>")
 # def hello(name = None):
